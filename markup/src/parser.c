0e3960fe4d4beb2605be6f7c47ef9bcf768db29c
António Mora <m00qek@gmail.com>
2026-06-18 22:29:29 +0200

    chore: regenerate parsers with minor_version = 5

--- markup/src/parser.c
+++ markup/src/parser.c
@@ -134501,7 +134501,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_ucode_markup(void) {
     .max_reserved_word_set_size = 28,
     .metadata = {
       .major_version = 0,
-      .minor_version = 4,
+      .minor_version = 5,
       .patch_version = 0,
     },
   };
--- ucdocs/src/parser.c
+++ ucdocs/src/parser.c
@@ -6376,7 +6376,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_ucdocs(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 4,
+      .minor_version = 5,
       .patch_version = 0,
     },
   };
