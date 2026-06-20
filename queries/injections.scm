a0ef2c3fee46aa851da30ff667e5e3a8aef1d4ca
António Mora <m00qek@gmail.com>
2026-06-20 19:12:00 +0200

    fix: tighten JSDoc injection regex to exclude /*** dividers
    The predicate ^/\*\* matched any comment starting with /**, including
    /*** section-divider banners.  Those comments are not JSDoc and their
    content was misinterpreted as tag/description structure.
    
    Add [^*] after the two required stars so only comments of the form
    /** … (space or non-star character immediately after the opener) are
    injected into the ucdocs grammar.

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,4 +1,5 @@
 ; Inject ucdocs into JSDoc block comments (/** ... */).
+; The [^*] guard excludes /*** section dividers which start with three or more stars.
 ((comment) @injection.content
-  (#match? @injection.content "^/\\*\\*")
+  (#match? @injection.content "^/\\*\\*[^*]")
   (#set! injection.language "ucdocs"))
